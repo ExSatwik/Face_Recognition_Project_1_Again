5d152b4ff270ded9f50026d0284db55d3bf12f98
ExSatwik <satwiksoni005@gmail.com>
2025-07-24 14:20:53 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-# Upgrade pip to avoid outdated install issues
+# Recommended by Render: always upgrade pip first
 pip install --upgrade pip
 
-# Install required dependencies
+# Sometimes helps with pyarrow / opencv related issues
+apt-get update && apt-get install -y cmake libgl1-mesa-glx libglib2.0-0
+
+# Install Python packages
 pip install -r requirements.txt
